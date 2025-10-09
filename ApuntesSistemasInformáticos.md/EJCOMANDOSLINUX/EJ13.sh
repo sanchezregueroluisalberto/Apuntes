d7c139cf1d4c6c8c789eb3863e196e0b8b329582
sanchezregueroluisalberto <sanchezregueroluisalberto001.alu@stemgranada.com>
2025-10-09 19:21:25 +0200

    -

---
+++ ApuntesSistemasInformáticos.md/EJCOMANDOSLINUX/EJ13.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cp /etc/magic PRUEBA/mensaje
---
+++ ApuntesSistemasInformáticos.md/EJCOMANDOSLINUX/Ej12.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir PRUEBA
+mkdir PRUEBA/dir1 PRUEBA/dir2 PRUEBA/dir3
+mkdir PRUEBA/dir1/dir11 PRUEBA/dir3/dir31
+mkdir PRUEBA/dir3/dir31/311 PRUEBA/dir3/dir31/dir312
