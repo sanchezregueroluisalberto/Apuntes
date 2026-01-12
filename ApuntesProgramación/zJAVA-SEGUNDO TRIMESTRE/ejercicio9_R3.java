74c3cff982bcb77eaa6dbd2185b4f6b26756b1bc
sanchezregueroluisalberto <sanchezregueroluisalberto001.alu@stemgranada.com>
2026-01-12 12:53:18 +0100

    ej9 R3

---
+++ ApuntesProgramación/zJAVA-SEGUNDO TRIMESTRE/ejercicio9_R3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ejercicio9_R3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un numero de dos cifras: ");
+        int numeroDosCifras = scanner.nextInt();
+
+        int cifra1 = (numeroDosCifras / 10);
+        int cifra2 = (numeroDosCifras - (cifra1 * 10));
+        int sumaCifras = cifra1 + cifra2;
+        scanner.nextLine();
+
+        System.out.print("La suma de la dos cifras es; " + sumaCifras);
+        scanner.close();
+    }
+}
