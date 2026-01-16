923e122c05586da0d0fe604c5cf06d4a8a8aa915
sanchezregueroluisalberto <sanchezregueroluisalberto001.alu@stemgranada.com>
2026-01-16 16:34:14 +0100

    ej 19 R0

---
+++ ApuntesProgramación/SEGUNDO TRIMESTRE/ejercicio19_R0.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ejercicio19_R0 {
+
+public static String capitalizarPrimeraLetra(String palabra) {
+    // declaramos vacia la palabra capitalizada
+    String palabraCapitalizada = "";
+    //bucle para ver si es la primera letra o no 
+    for (int i = 0; i < palabra.length(); i++) {
+        char letra = palabra.charAt(i);
+        // si es la primera letra la capitalizamos si no no
+        if (i == 0) {
+            palabraCapitalizada += Character.toUpperCase(letra);
+        } else{
+            palabraCapitalizada += letra;
+        }
+    }
+    return palabraCapitalizada;
+}
+// MAIN--------------------------------------------
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // pedimos una palabra
+        System.out.println("Introduzca una palabra");
+
+        String palabra = scanner.nextLine();
+        // enunciamos la función
+        String palabraParaCapitalizar = capitalizarPrimeraLetra(palabra);
+        // devolvemos la palabra capitalizada
+        System.out.println("La palabra capitalizada es: " + palabraParaCapitalizar);
+        scanner.close();
+    }
+}
