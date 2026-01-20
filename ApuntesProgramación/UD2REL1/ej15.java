7024d1248e58140ea4cea0a17a1f19db54f9d03f
sanchezregueroluisalberto <sanchezregueroluisalberto001.alu@stemgranada.com>
2026-01-20 20:11:12 +0100

    ej15

--- ApuntesProgramación/UD2REL1/ej15.java
+++ ApuntesProgramación/UD2REL1/ej15.java
@@ -1,5 +1,21 @@
 package ApuntesProgramación.UD2REL1;
 
+import java.util.ArrayList;
+
 public class ej15 {
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        System.out.println("El array antes de eliminar nada es de: " + numeros);
+        for (int i = numeros.size(); i > 0; i--) {
+            numeros.remove(numeros.size() - 1);
+            System.out.println(numeros);
+
+        }
+
+    }
 }
