d5a2484638edba4cb9f1dbf59bc3dc8c4ae71735
sanchezregueroluisalberto <sanchezregueroluisalberto001.alu@stemgranada.com>
2026-02-10 20:04:23 +0100

    .

--- ApuntesProgramación/SEGUNDOTRIMESTRE/REPASOJAVA/constructores/ej4Constructores.java
+++ ApuntesProgramación/SEGUNDOTRIMESTRE/REPASOJAVA/constructores/ej4Constructores.java
@@ -2,32 +2,32 @@ package ApuntesProgramación.SEGUNDOTRIMESTRE.REPASOJAVA.constructores;
 
 public class ej4Constructores {
     public static void main(String[] args) {
-     auto micoche = new auto("Peugeot", "Nuevo", 150.00); 
-     System.out.println(micoche.velocidadActual);
-     micoche.acelerar(25.00);
-     System.out.println(micoche.velocidadActual);
-     micoche.frenar(50);
+        Auto micoche = new Auto("Peugeot", "Nuevo", 150.00);
+        System.out.println(micoche.velocidadActual);
+        micoche.acelerar(25.00);
+        System.out.println(micoche.velocidadActual);
+        micoche.frenar(50);
+        System.out.println(micoche.velocidadActual);
     }
 
 }
 
-class auto {
-    String marca = "";
-    String modelo = "";
-    double velocidadActual = 0.00;
+class Auto {
+    String marca;
+    String modelo;
+    double velocidadActual;
 
-    auto(String marca,String modelo,double velocidadActual){
+    Auto(String marca, String modelo, double velocidadActual) {
         this.marca = marca;
         this.modelo = modelo;
         this.velocidadActual = velocidadActual;
     }
-    void acelerar(double cantidad){
-        cantidad += velocidadActual;
-        System.out.println("Tranquiiilo que estas acelerando " + cantidad + ", estas a " + velocidadActual + "km/h");
+
+    void acelerar(double cantidad) {
+        velocidadActual+= cantidad; 
     }
 
-    void frenar(double cantidad){
-        cantidad -= velocidadActual;
-        System.out.println("Menos mal que estas frenando " + cantidad + ", estas a " + velocidadActual + "km/h");
+    void frenar(double cantidad) {
+        velocidadActual -= cantidad;
     }
 }
---
+++ ApuntesSistemasInformáticos/SEGUNDOTRIMESTRE/funciones/ejercicios.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+DIVISION_ENTERA() { 
+resultado=$(( $1 / $2 ))
+echo $resultado
+} 
+resultado=$( DIVISION_ENTERA 7 5 )
+echo $resultado
+ 
+area_circulo() { 
+
+} 
+perimetro_circunferencia() { 
+
+} 
+volumen_esfera() { 
+
+} 
+ area_rectangulo() { 
+
+} 
+
+division_entera 7 5
+perimetro_circulo 2
+area_circulo 2
+area_rectangulo 2 7
+ 
